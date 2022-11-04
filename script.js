57c1978b9573dc6887d0ef70c141a372e4e5d8c4
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-04 23:21:36 +0300

    addEventListener finished

--- script.js
+++ script.js
@@ -74,3 +74,25 @@ function getSelected() {
 
     return answer
 }
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+                <button onclick="location.reload()">Reload</button>
+            `
+        }
+    }
+})
