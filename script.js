6d412d18825e9492d84fd34c7ce38eb701b4585c
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-04 23:20:37 +0300

    sellectors finished

--- script.js
+++ script.js
@@ -32,3 +32,12 @@ const quizData = [
         correct: "b",
     },
 ];
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
